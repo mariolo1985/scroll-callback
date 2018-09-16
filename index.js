bc60d6756782395deb70adc9f5445ab6b0aaa665
mariolo1985 <loserpunkx182@gmail.com>
2018-09-16 09:30:23 -0600

    lowercase

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-module.exports.scrollCallback = require('./src/Scrollers/scrollCallback');
-module.exports.scrollCallbackWithElement = require('./src/Scrollers/scrollCallbackWithElement');
-module.exports.callbackAtElement = require('./src/Scrollers/callbackAtElement');
+module.exports.scrollCallback = require('./src/scrollers/scrollCallback');
+module.exports.scrollCallbackWithElement = require('./src/scrollers/scrollCallbackWithElement');
+module.exports.callbackAtElement = require('./src/scrollers/callbackAtElement');
 
 // Utils
 module.exports.getClass = require('./src/utils/getClass');
