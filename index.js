cbe4cc230adb7b02a47b2d402aa36a5676187ef0
mariolo1985 <loserpunkx182@gmail.com>
2018-10-06 22:43:12 -0600

    added callbackAtElementSurface

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports.scrollCallback = require('./src/scrollcaller/scrollCallback');
 module.exports.scrollCallbackWithElement = require('./src/scrollcaller/scrollCallbackWithElement');
 module.exports.callbackAtElement = require('./src/scrollcaller/callbackAtElement');
+module.exports.callbackAtElementSurface = require('./src/scrollcaller/callbackAtElementSurface');
 
 // Utils
 module.exports.getClass = require('./src/utils/getClass');
